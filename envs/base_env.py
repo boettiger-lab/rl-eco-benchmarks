7045b5652beb8ffa218cab907db13879dc3d585a
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-08-15 14:33:04 -0700

    mini

--- envs/base_env.py
+++ envs/base_env.py
@@ -58,7 +58,7 @@ class eco_env(gym.Env):
 		# reset
 		self.reset()
 
-	def reset(self):
+	def reset(self, *, seed=42):
 		self.timestep = 0
 		reset_pert = self.reset_sigma * np.random.normal(size=3)
 		self.pop = self.init_pop + reset_pert
