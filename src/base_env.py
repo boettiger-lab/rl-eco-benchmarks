43629a75799ce038345fcb18fe94b3600cb6973f
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-08-22 14:28:13 -0700

    typo

--- src/base_env.py
+++ src/base_env.py
@@ -161,7 +161,7 @@ class ray_eco_env(gym.Env):
 		self.check_config()
 		self.env = eco_env(
 			metadata=self.config['metadata'],
-			dyn_fn=self.config.['dyn_fn'],
+			dyn_fn=self.config['dyn_fn'],
 			dyn_params=self.config.get('dyn_params', {}),
 			non_stationary=self.config.get('non_stationary', False),
 			non_stationarities=self.config.get('non_stationarities', {}),
