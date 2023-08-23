d261625ab95ecb20f1fad4c8219f56f6402dffeb
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-08-22 21:30:23 -0700

    lower cpus, higher gpus

--- src/ray_trainer_api.py
+++ src/ray_trainer_api.py
@@ -53,8 +53,8 @@ class ray_trainer:
 		self.cpus_to_use = os.cpu_count() // 2
 		self.algo_config = self.algo_config.resources(
 			num_gpus=torch.cuda.device_count(), 
-			num_gpus_per_worker=0.5,
-			num_cpus_per_worker=16,
+			num_gpus_per_worker=1,
+			num_cpus_per_worker=3,
 		)
 		# 
 		# config.env
