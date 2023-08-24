e991ac19b1eb892ea24f5bbdee3ee4f4df0fe0ba
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-08-24 12:57:04 -0700

    dynamics

--- src/ex_script.py
+++ src/ex_script.py
@@ -215,7 +215,7 @@ params = {
 	"K": np.float32(1),
 	"beta": np.float32(0.3),
 	"v0":  np.float32(0.1),
-	"D": np.float32(-0.2),
+	"D": np.float32(-0.9),
 	"tau_yx": np.float32(0),
 	"tau_xy": np.float32(0),
 	"alpha": np.float32(1), 
