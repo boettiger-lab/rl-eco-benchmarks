91be23c5446e054835eff65c0a5a8ead3ff1fc32
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-09-06 16:35:28 -0700

    debugging

--- src/env_factories.py
+++ src/env_factories.py
@@ -2,11 +2,14 @@
 # factory for standard choices of env based on # species and actions
 #
 from base_env import eco_env, ray_eco_env
+from util import dict_pretty_print
 
 def env_factory(**env_config):
 	return eco_env(**env_config)
 
 def ray_env_factory(env_config):
-	print("\n ############ \nENV CONFIG:\n\n", env_config, "\n############")
+	print("\n ############ \nENV CONFIG:\n\n")
+	dict_pretty_print(env_config)
+	print("############")
 	assert False, "was here."
 	return eco_env(**env_config)
