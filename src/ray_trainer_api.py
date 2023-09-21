b07e2c886da0a1ad6e33cfa2a8528629e2037ca1
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-09-21 12:40:12 -0700

    ok now it's not doing the 100% cpu usage, crank up num workers, keep cpu/worker low

--- src/ray_trainer_api.py
+++ src/ray_trainer_api.py
@@ -56,20 +56,20 @@ class ray_trainer:
 			self.algo_config = self.algo_config.resources(
 				num_gpus=min(1, torch.cuda.device_count()), 
 				num_gpus_per_worker=0.5,
-				num_cpus_per_worker=4,
+				num_cpus_per_worker=3,
 			)
 		elif algo_name == "ddppo":
 			# no gpus for ddppo since all the parallelization happens inside workers
 			self.algo_config = self.algo_config.resources(
 				num_gpus=0, 
 				num_gpus_per_worker=0.5,
-				num_cpus_per_worker=4,
+				num_cpus_per_worker=3,
 			)
 		else:
 			self.algo_config = self.algo_config.resources(
 				num_gpus=torch.cuda.device_count(), 
 				num_gpus_per_worker=0.5,
-				num_cpus_per_worker=4,
+				num_cpus_per_worker=3,
 			)
 		# 
 		# config.env
