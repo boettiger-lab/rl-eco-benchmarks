5f91d18b9b948392afcb74058d72aaefc23c854a
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-10-02 20:01:22 -0700

    debug

--- src/hp_tuning_script.py
+++ src/hp_tuning_script.py
@@ -193,8 +193,8 @@ RT = ray_trainer(
 
 tuning_df = RT.tune_hyper_params(
 	hp_dicts_list=hyperparameters[algo],
-	num_workers=2,
-	num_samples=2,
+	num_workers=7,
+	num_samples=7,
 	criteria_max=500_000,
 	)
 
