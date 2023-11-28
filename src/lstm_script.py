beca7a208fdffcb1a3f82b7cc784308cff6b8cc0
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-11-27 16:41:13 -0800

    debug

--- src/lstm_script.py
+++ src/lstm_script.py
@@ -99,56 +99,72 @@ problem_summary = {
 			}
 
 import base_env
-env = base_env.eco_env(**problem_summary)
 
-train_config = {
-	"env": env,
-	# "rl_module":{"_enable_rl_module_api": False},
-	# "training": {"_enable_learner_api": False},
-	"model": {
-		"vf_share_layers": True,
-		"custom_model": "frame_stack_model",
-		"custom_model_config": {
-			"num_frames": 10,
-		},
-		# "use_lstm": True,
-		# "lstm_cell_size": 64,
-		# "max_seq_len": 10,
-		# "lstm_use_prev_action": True,
-	},
-	"num_sgd_iter": 5,
-	"vf_loss_coeff": 0.0001,
-	"framework": "torch",
-}
 
-# ###################################
-# ######## INIT & TRAIN ALGO ########
-# ###################################
 
-from ray import tune
+
+import numpy as np
+
 import ray
+import ray.rllib.algorithms.ppo as ppo
+from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
+from ray.rllib.models.catalog import ModelCatalog
+from ray.rllib.utils.framework import try_import_torch
 
-ray.init(num_cpus=3)
+torch, _ = try_import_torch()
 
-ModelCatalog.register_custom_model(
-	"frame_stack_model", TorchFrameStackingCartPoleModel
-)
+# __sphinx_doc_begin__
 
-stop = {
-	"training_iteration": 200,
-}
 
-results = tune.run(
-	"PPO", config=train_config, stop=stop, verbose=2, checkpoint_at_end=True
-)
+# The custom model that will be wrapped by an LSTM.
+class MyCustomModel(TorchModelV2):
+    def __init__(self, obs_space, action_space, num_outputs, model_config, name):
+        super().__init__(obs_space, action_space, num_outputs, model_config, name)
+        self.num_outputs = int(np.product(self.obs_space.shape))
+        self._last_batch_size = None
 
-# ITERATIONS = 100
-# ALGO = "ppo"
+    # Implement your own forward logic, whose output will then be sent
+    # through an LSTM.
+    def forward(self, input_dict, state, seq_lens):
+        obs = input_dict["obs_flat"]
+        # Store last batch size for value_function output.
+        self._last_batch_size = obs.shape[0]
+        # Return 2x the obs (and empty states).
+        # This will further be sent through an automatically provided
+        # LSTM head (b/c we are setting use_lstm=True below).
+        return obs * 2.0, []
 
-# RT = ray_trainer( # wrapper class around ray RLLib algorithms.
-# 	algo_name=ALGO, 
-# 	config=problem_summary,
-# 	train_cfg=train_config,
-# )
-# agent = RT.train(iterations=ITERATIONS)
+    def value_function(self):
+        return torch.from_numpy(np.zeros(shape=(self._last_batch_size,)))
 
+
+if __name__ == "__main__":
+    ray.init()
+
+    # Register the above custom model.
+    ModelCatalog.register_custom_model("my_torch_model", MyCustomModel)
+
+    # Create the Algorithm from a config object.
+    config = (
+        ppo.PPOConfig()
+        .environment(base_env.ray_eco_env)
+        .env_config(problem_summary)
+        .framework("torch")
+        .training(
+            model={
+                # Auto-wrap the custom(!) model with an LSTM.
+                "use_lstm": True,
+                # To further customize the LSTM auto-wrapper.
+                "lstm_cell_size": 64,
+                # Specify our custom model from above.
+                # "custom_model": "my_torch_model",
+                # Extra kwargs to be passed to your model's c'tor.
+                # "custom_model_config": {},
+            }
+        )
+    )
+    algo = config.build()
+    algo.train()
+    algo.stop()
+
+# __sphinx_doc_end__
